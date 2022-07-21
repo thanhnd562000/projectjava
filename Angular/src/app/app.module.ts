fc9d2e0c8d77f1be04379bb24570a83f3b8fd78b
thanhnd562000 <tn258806@gmail.com>
2022-07-21 23:00:10 +0700

    show product

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './features/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './core/services/product.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { ProductListComponent } from './features/product-list/product-list.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular/src/app/features/product-list/product-list.component.ts
+++ Angular/src/app/features/product-list/product-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/core/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  products: Product[];
+  constructor(private productServices: ProductService) {}
 
   ngOnInit(): void {
+   this.ListProducts();
   }
-
+  ListProducts()
+  {
+    this.productServices.getProductList().subscribe((res) => {
+      this.products = res;
+    });
+  }
 }
