3203e8f5ae60cba8a95ed2beaccf2721e3f0403b
thanhnd562000 <tn258806@gmail.com>
2022-07-21 08:41:25 +0700

    create project

---
+++ Java/src/test/java/com/javasb/springbootecommerce/JavaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.javasb.springbootecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
