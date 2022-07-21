47c76cb77d49b46399ab040f7b8069a8c86769b8
thanhnd562000 <tn258806@gmail.com>
2022-07-21 11:17:25 +0700

    create productcategoryrepository

--- Java/src/main/java/com/javasb/springbootecommerce/dao/ProductRespository.java
+++ Java/src/main/java/com/javasb/springbootecommerce/dao/ProductRespository.java
@@ -3,9 +3,8 @@ package com.javasb.springbootecommerce.dao;
 import com.javasb.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRespository extends JpaRepository<Product,Long> {
-/*
-* package-> new interface.
- */
+/**
+ * package-> new interface (entity,key).
+*/
 
-}
+public interface ProductRespository extends JpaRepository<Product, Long> {}
