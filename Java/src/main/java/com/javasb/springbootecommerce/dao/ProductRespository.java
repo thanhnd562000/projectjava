b07c9734e695f54dfe5f5873e5519ad72bf19bbc
thanhnd562000 <tn258806@gmail.com>
2022-07-22 00:16:53 +0700

    enable cors

--- Java/src/main/java/com/javasb/springbootecommerce/dao/ProductRespository.java
+++ Java/src/main/java/com/javasb/springbootecommerce/dao/ProductRespository.java
@@ -2,9 +2,10 @@ package com.javasb.springbootecommerce.dao;
 
 import com.javasb.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * package-> new interface (entity,key).
 */
-
+@CrossOrigin("http://localhost:4200")
 public interface ProductRespository extends JpaRepository<Product, Long> {}
