138369bd3200b3b7b227e10fce8caec952624251
thanhnd562000 <tn258806@gmail.com>
2022-07-23 22:35:27 +0700

    create module, component cart

--- Angular/src/app/core/services/product.service.ts
+++ Angular/src/app/core/services/product.service.ts
@@ -12,63 +12,78 @@ export class ProductService {
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-
-  getProductListPaginate(thePage: number, thePageSize:number,theCategoryId:number): Observable<GetResponseProducts> {
-
-    // need to build URL based on category id ,pagesize, 
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
-    +`&page=${thePage}&size=${thePageSize}`;
+  getProductListPaginate(
+    thePage: number,
+    thePageSize: number,
+    theCategoryId: number
+  ): Observable<GetResponseProducts> {
+    // need to build URL based on category id ,pagesize,
+    const searchUrl =
+      `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}` +
+      `&page=${thePage}&size=${thePageSize}`;
 
     return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-
-    // need to build URL based on category id 
+    // need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.GetProducts(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
-
-    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
-      map(response => response._embedded.productCategory)
-    );
+    return this.httpClient
+      .get<GetResponseProductCategory>(this.categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
   }
+
+  //search product
   searchProducts(theKeyword: string): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
     return this.GetProducts(searchUrl);
   }
+  searchProductPaginate(
+    thePage: number,
+    thePageSize: number,
+    theKeyword: string
+  ): Observable<GetResponseProducts> {
+    // need to build URL based on category id ,pagesize,
+    const searchUrl =
+      `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}` +
+      `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   getProduc(theProductId: number): Observable<Product> {
     const productUrl = `${this.baseUrl}/${theProductId}`;
     return this.httpClient.get<Product>(productUrl);
   }
 
   private GetProducts(searchUrl: string): Observable<Product[]> {
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    return this.httpClient
+      .get<GetResponseProducts>(searchUrl)
+      .pipe(map((response) => response._embedded.products));
   }
 }
 
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
-  },
-  page:{
-    size:number,//size page
-    totalElements:number,//tổng bản ghi
-    totalPages:number,//tổng số trang
-    number:number//trang 
-  }
+  };
+  page: {
+    size: number; //size page
+    totalElements: number; //tổng bản ghi
+    totalPages: number; //tổng số trang
+    number: number; //trang
+  };
 }
 
 interface GetResponseProductCategory {
   _embedded: {
     productCategory: ProductCategory[];
-  }
+  };
 }
-
--- Angular/src/app/features/product-list/product-list.component.ts
+++ Angular/src/app/features/product-list/product-list.component.ts
@@ -10,10 +10,10 @@ import { ProductService } from 'src/app/core/services/product.service';
 })
 export class ProductListComponent implements OnInit {
   products: Product[];
-  currentCategoryId: number=1;
-  searchMode:boolean;
-  previousCategoryId:number=1;
-
+  currentCategoryId: number = 1;
+  searchMode: boolean;
+  previousCategoryId: number = 1;
+  previousKeyword: any =null;
   thePageNumber: number = 1;
   thePageSize: number = 8;
   theTotalElements: number = 0;
@@ -28,57 +28,69 @@ export class ProductListComponent implements OnInit {
     });
   }
   listProducts() {
-
     this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
     if (this.searchMode) {
       this.handleSearchProducts();
-    }
-    else {
+    } else {
       this.handleListProducts();
     }
-
   }
 
   handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+    //if we have a different keyword than previous
+    //then set the pagenumber=1;
 
-    const theKeyword : string = this.route.snapshot.paramMap.get('keyword')!;
-
+    if (this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+    this.previousKeyword = theKeyword;
+    console.log(
+      `previousKeyword=${this.previousKeyword}, thePageNumber=${this.thePageNumber}`
+    );
+    this.productServices
+      .searchProductPaginate(
+        this.thePageNumber - 1,
+        this.thePageSize,
+        theKeyword
+      )
+      .subscribe(this.processResult);
     // now search for the products using keyword
-    this.productServices.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productServices.searchProducts(theKeyword).subscribe((data) => {
+      this.products = data;
+    });
   }
 
   handleListProducts() {
     //
 
     const hasCategoryId: boolean = this.route.snapshot.params['id'];
-   if(hasCategoryId)
-   {
-    this.currentCategoryId=+ this.route.snapshot.params['id'];
-   }
-   else{
-    this.currentCategoryId=1;
-   }
-//
-if(this.previousCategoryId!=this.currentCategoryId)
-{
-  this.thePageNumber=1;
-}
-this.previousCategoryId=this.currentCategoryId
-console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.params['id'];
+    } else {
+      this.currentCategoryId = 1;
+    }
+    //
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+    this.previousCategoryId = this.currentCategoryId;
+    console.log(
+      `currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`
+    );
 
-// now get the products for the given category id
-this.productServices.getProductListPaginate(this.thePageNumber - 1,
-                                           this.thePageSize,
-                                           this.currentCategoryId)
-                                           .subscribe(this.processResult());
+    // now get the products for the given category id
+    this.productServices
+      .getProductListPaginate(
+        this.thePageNumber - 1,
+        this.thePageSize,
+        this.currentCategoryId
+      )
+      .subscribe(this.processResult());
   }
   processResult() {
-    return (data:any) => {
+    return (data: any) => {
       this.products = data._embedded.products;
       this.thePageNumber = data.page.number + 1;
       this.thePageSize = data.page.size;
