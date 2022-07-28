59756b8b13022786e179c0afee16be3b141c3a7c
thanhnd562000 <tn258806@gmail.com>
2022-07-28 22:47:36 +0700

    create model

--- Angular/src/app/common/order-item.model.ts
+++ Angular/src/app/common/order-item.model.ts
@@ -1,2 +1,15 @@
+import { Cart } from './cart.model';
+
 export class OrderItem {
+  imageUrl: string;
+  unitPrice: number;
+  quantity: number;
+  productId: string;
+
+  constructor(cartItem: Cart) {
+    this.imageUrl = cartItem.imageUrl;
+    this.quantity = cartItem.quantity;
+    this.productId = cartItem.id;
+    this.unitPrice = cartItem.unitPrice;
+  }
 }
--- Angular/src/app/common/purchase.model.ts
+++ Angular/src/app/common/purchase.model.ts
@@ -1,2 +1,12 @@
+import { Address } from './address.model';
+import { Customer } from './customer';
+import { OrderItem } from './order-item.model';
+import { Order } from './order.model';
+
 export class Purchase {
+  customer: Customer;
+  shippingAddress: Address;
+  billingAddress: Address;
+  order: Order;
+  orderItems: OrderItem[];
 }
